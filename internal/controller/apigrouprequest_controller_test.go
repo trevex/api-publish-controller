e9212ad24b16e4b56799de55de8b1ca378248917
Christian Bendieck <chris.bendieck@gmail.com>
2024-12-09 16:04:27 +0100

    completing tests for APIGroupController

--- internal/controller/apigrouprequest_controller_test.go
+++ internal/controller/apigrouprequest_controller_test.go
@@ -21,6 +21,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	gomegatypes "github.com/onsi/gomega/types"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -29,23 +30,19 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	apiv1alpha1 "github.com/trevex/api-publish-controller/api/v1alpha1"
+
+	apimachinerymeta "k8s.io/apimachinery/pkg/api/meta"
 )
 
 var _ = Describe("APIGroupRequest Controller", func() {
 
 	const (
-		resourceName        = "test-apigroup"
-		clusterApiGroupName = "test-clusterapigroup"
-		namespace           = "default"
+		resourceName = "test-apigroup"
+		namespace    = "default"
 	)
 
 	typeNamespacedName := types.NamespacedName{
 		Name:      resourceName,
-		Namespace: namespace,
-	}
-
-	namespacedNameClusterApiGroup := types.NamespacedName{
-		Name:      clusterApiGroupName,
 		Namespace: namespace,
 	}
 
@@ -75,7 +72,7 @@ var _ = Describe("APIGroupRequest Controller", func() {
 			err = k8sClient.Get(ctx, typeNamespacedName, clusterApiGroup)
 			if err != nil && errors.IsNotFound(err) {
 				clusterApiGroup.ObjectMeta = metav1.ObjectMeta{
-					Name: clusterApiGroupName,
+					Name: resourceName,
 				}
 				annotations := map[string]string{
 					requestNameAnnotation:      resourceName,
@@ -95,7 +92,7 @@ var _ = Describe("APIGroupRequest Controller", func() {
 		It("should successfully reconcile the resources", func() {
 
 			By("checking if needed resources are there")
-			Expect(k8sClient.Get(ctx, namespacedNameClusterApiGroup, clusterApiGroup)).To(Succeed())
+			Expect(k8sClient.Get(ctx, typeNamespacedName, clusterApiGroup)).To(Succeed())
 			Expect(k8sClient.Get(ctx, typeNamespacedName, apiGroupRequest)).To(Succeed())
 
 			By("reconciling the created resource")
@@ -120,48 +117,121 @@ var _ = Describe("APIGroupRequest Controller", func() {
 		})
 	})
 
-	// Context("reconciling a resource ...", func() {
+	Context("reconciling a resource", func() {
 
-	// 	ctx := context.Background()
+		ctx := context.Background()
 
-	// 	BeforeEach(func() {
-	// 		By("creating the custom resource for the Kind APIGroupRequest")
-	// 		err := k8sClient.Get(ctx, typeNamespacedName, apigrouprequest)
-	// 		if err != nil && errors.IsNotFound(err) {
-	// 			resource := &apiv1alpha1.APIGroupRequest{
-	// 				ObjectMeta: metav1.ObjectMeta{
-	// 					Name:      resourceName,
-	// 					Namespace: "default",
-	// 				},
-	// 				// TODO(user): Specify other spec details if needed.
-	// 			}
-	// 			Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-	// 		}
-	// 	})
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind APIGroupRequest")
+			err := k8sClient.Get(ctx, typeNamespacedName, apiGroupRequest)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &apiv1alpha1.APIGroupRequest{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
 
-	// 	AfterEach(func() {
-	// 		// TODO(user): Cleanup logic after each test, like removing the resource instance.
-	// 		resource := &apiv1alpha1.APIGroupRequest{}
-	// 		err := k8sClient.Get(ctx, typeNamespacedName, resource)
-	// 		Expect(err).NotTo(HaveOccurred())
+		AfterEach(func() {
+			resource := &apiv1alpha1.APIGroupRequest{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
 
-	// 		By("Cleanup the specific resource instance APIGroupRequest")
-	// 		Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-	// 	})
+			By("Cleanup the specific resource instance APIGroupRequest")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
 
-	// 	It("should successfully reconcile the resource", func() {
-	// 		By("Reconciling the created resource")
-	// 		controllerReconciler := &APIGroupRequestReconciler{
-	// 			Client: k8sClient,
-	// 			Scheme: k8sClient.Scheme(),
-	// 		}
+			By("Reconciling the resource to delete it successfully")
+			reconcileResource(typeNamespacedName)
 
-	// 		_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-	// 			NamespacedName: typeNamespacedName,
-	// 		})
-	// 		Expect(err).NotTo(HaveOccurred())
-	// 		// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-	// 		// Example: If you expect a certain status condition after reconciliation, verify it here.
-	// 	})
-	// })
+			By("Checking, if resource is gone after deleting it")
+			err = k8sClient.Get(ctx, typeNamespacedName, apiGroupRequest)
+			Expect(errors.IsNotFound(err)).To(BeTrue())
+		})
+
+		It("should successfully reconcile the resource, if a ClusterAPIGroup owned by someone else exists", func() {
+			By("Creating a ClusterAPIGroup owned by someone else")
+			cagr := &apiv1alpha1.ClusterAPIGroup{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: resourceName,
+					Annotations: map[string]string{
+						requestNameAnnotation:      "someOtherName",
+						requestNamespaceAnnotation: "someOtherNamespace",
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, cagr)).To(Succeed())
+
+			By("Reconciling the created resource")
+			reconcileResource(typeNamespacedName)
+
+			By("Checking, if status of APIGroupRequest resource is set correctly")
+			agr := &apiv1alpha1.APIGroupRequest{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, agr)).To(Succeed())
+			checkStatus(agr.Status.Conditions, "APIGroupReserved", "ClusterAPIGroupOwnedByOthers", BeFalse())
+
+			By("Deleting ClusterAPIGroup again, which is not owned by us")
+			Expect(k8sClient.Delete(ctx, cagr)).To(Succeed())
+		})
+
+		It("should successfully reconcile the resource, if ClusterAPIGroup already exists and is owned by us", func() {
+			By("Creating a ClusterAPIGroup owned by us")
+			cagr := &apiv1alpha1.ClusterAPIGroup{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: resourceName,
+					Annotations: map[string]string{
+						requestNameAnnotation:      resourceName,
+						requestNamespaceAnnotation: namespace,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, cagr)).To(Succeed())
+
+			By("Reconciling the created resource")
+			reconcileResource(typeNamespacedName)
+
+			By("Checking, if status of APIGroupRequest resource is set correctly")
+			agr := &apiv1alpha1.APIGroupRequest{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, agr)).To(Succeed())
+			checkStatus(agr.Status.Conditions, "APIGroupReserved", "ClusterAPIGroupAlreadyOwned", BeTrue())
+		})
+
+		It("should successfully reconcile the resource, if ClusterAPIGroup does not already exist", func() {
+			By("Reconciling the created resource")
+			reconcileResource(typeNamespacedName)
+
+			By("Checking, if corresponding ClusterAPIGroup exists")
+			cagr := &apiv1alpha1.ClusterAPIGroup{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, cagr)).To(Succeed())
+
+			By("Checking, if corresponding ClusterAPIGroup has correct owner annotations")
+			ownedByUs, _ := isOwned(cagr, typeNamespacedName)
+			Expect(ownedByUs).To(BeTrue())
+
+			By("Checking, if status of APIGroupRequest resource is set correctly")
+			agr := &apiv1alpha1.APIGroupRequest{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, agr)).To(Succeed())
+			checkStatus(agr.Status.Conditions, "APIGroupReserved", "ClusterAPIGroupCreated", BeTrue())
+		})
+	})
 })
+
+func reconcileResource(namespacedName types.NamespacedName) {
+	controllerReconciler := &APIGroupRequestReconciler{
+		Client: k8sClient,
+		Scheme: k8sClient.Scheme(),
+	}
+
+	_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+		NamespacedName: namespacedName,
+	})
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func checkStatus(conditions []metav1.Condition, conditionType, conditionReason string, matcher gomegatypes.GomegaMatcher) {
+	apiGroupReservedCondition := apimachinerymeta.FindStatusCondition(conditions, conditionType)
+	Expect(apimachinerymeta.IsStatusConditionTrue(conditions, conditionType)).To(matcher)
+	Expect(apiGroupReservedCondition.Reason).To(Equal(conditionReason))
+}
